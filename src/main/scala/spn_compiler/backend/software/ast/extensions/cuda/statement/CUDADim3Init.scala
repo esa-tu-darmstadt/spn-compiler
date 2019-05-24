5c2cbaaf75a6fc88c9ca41fa7a67347e0c350920
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2019-05-24 15:33:04 +0200

    Include variable in dim3 initialization statement;

--- src/main/scala/spn_compiler/backend/software/ast/extensions/cuda/statement/CUDADim3Init.scala
+++ src/main/scala/spn_compiler/backend/software/ast/extensions/cuda/statement/CUDADim3Init.scala
@@ -1,14 +1,17 @@
 package spn_compiler.backend.software.ast.extensions.cuda.statement
 
+import spn_compiler.backend.software.ast.extensions.cuda.predef.CUDADim3Type
 import spn_compiler.backend.software.ast.nodes.statement.ASTStatement
 import spn_compiler.backend.software.ast.nodes.types.IntegerType
 import spn_compiler.backend.software.ast.nodes.value.ASTValue
 import spn_compiler.backend.software.ast.nodes.value.constant.ASTConstant
+import spn_compiler.backend.software.ast.nodes.variable.ASTVariable
 
-class CUDADim3Init private[ast](val x : ASTValue = new ASTConstant(IntegerType, 1),
+class CUDADim3Init private[ast](val variable : ASTVariable,
+                                val x : ASTValue = new ASTConstant(IntegerType, 1),
                                 val y : ASTValue = new ASTConstant(IntegerType, 1),
                                 val z : ASTValue = new ASTConstant(IntegerType, 1)) extends ASTStatement{
-
+  require(variable.getType == CUDADim3Type)
   require(x.getType == IntegerType)
   require(y.getType == IntegerType)
   require(z.getType == IntegerType)
@@ -17,6 +20,7 @@ class CUDADim3Init private[ast](val x : ASTValue = new ASTConstant(IntegerType, 
 
 object CUDADim3Init {
 
-  def unapply(arg: CUDADim3Init): Option[(ASTValue, ASTValue, ASTValue)] = Some(arg.x, arg.y, arg.z)
+  def unapply(arg: CUDADim3Init): Option[(ASTVariable, ASTValue, ASTValue, ASTValue)] =
+    Some(arg.variable,arg.x, arg.y, arg.z)
 
 }
