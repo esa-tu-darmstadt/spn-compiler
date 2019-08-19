bdceaee2e90624bd621247bd7c5a495ed559d9ea
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2019-08-19 13:12:17 +0200

    Simple infrastructure to add annotations on AST nodes;

--- src/main/scala/spn_compiler/backend/software/ast/nodes/ASTNode.scala
+++ src/main/scala/spn_compiler/backend/software/ast/nodes/ASTNode.scala
@@ -1,3 +1,16 @@
 package spn_compiler.backend.software.ast.nodes
 
-abstract class ASTNode
+import scala.collection.mutable
+
+abstract class ASTNode {
+
+  private val annotations : mutable.Map[String, Any] = mutable.Map()
+
+  def addAnnotation(key : String, value : Any) : this.type = {
+    annotations += key -> value
+    this
+  }
+
+  def getAnnotation(key : String) : Option[Any] = annotations.get(key)
+  
+}
--- src/main/scala/spn_compiler/backend/software/ast/nodes/value/ASTValue.scala
+++ src/main/scala/spn_compiler/backend/software/ast/nodes/value/ASTValue.scala
@@ -1,8 +1,9 @@
 package spn_compiler.backend.software.ast.nodes.value
 
+import spn_compiler.backend.software.ast.nodes.ASTNode
 import spn_compiler.backend.software.ast.nodes.types.ASTType
 
-abstract class ASTValue {
+abstract class ASTValue extends ASTNode{
 
   def getType : ASTType
 
