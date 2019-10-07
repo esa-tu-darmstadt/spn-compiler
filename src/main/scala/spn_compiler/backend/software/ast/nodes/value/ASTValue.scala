85f3123fc5d519eaa2fd4d767c9c8f0431d6ba5c
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2019-10-07 15:34:01 +0200

    Clean up for fresh restart using C++;

--- src/main/scala/spn_compiler/backend/software/ast/nodes/value/ASTValue.scala
+++
@@ -1,9 +0,0 @@
-package spn_compiler.backend.software.ast.nodes.value
-
-import spn_compiler.backend.software.ast.nodes.types.ASTType
-
-abstract class ASTValue {
-
-  def getType : ASTType
-
-}
--- src/main/scala/spn_compiler/backend/software/ast/predef/ASTCompilerRuntimePredef.scala
+++
@@ -1,9 +0,0 @@
-package spn_compiler.backend.software.ast.predef
-
-import spn_compiler.backend.software.ast.nodes.function.{ASTExternalFunction, ASTExternalHeader}
-import spn_compiler.backend.software.ast.nodes.types.RealType
-
-case object CompilerRuntimeHeader extends ASTExternalHeader("spn-compiler-rt.hpp", true)
-
-case object RegisterRange extends ASTExternalFunction(CompilerRuntimeHeader, "register_range",
-  RealType, RealType)
