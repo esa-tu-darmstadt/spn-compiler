07de96c186c0fdbbd97820f7cb4b9c29b471076c
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2019-12-05 13:48:25 +0100

    Refactoring: Remove indirection through runtime.cpp;

--- execute/src/main.cpp
+++ execute/src/main.cpp
@@ -3,16 +3,16 @@
 //
 
 #include <spnc.h>
-#include <runtime.h>
+#include <spnc-runtime.h>
 #include <iostream>
 
 int main(int argc, char* argv[]){
     auto parseResult = spnc::parseJSON(std::string(argv[1]));
     std::cout << "Parsed JSON? " << parseResult << std::endl;
-    auto kernel = loadKernel("/home/wimi/ls/Code/SPN/spn-compiler-v2/cmake-build-debug/execute/libdynamic-load-test.so", "foo");
+    Kernel kernel("/home/wimi/ls/Code/SPN/spn-compiler-v2/cmake-build-debug/execute/libdynamic-load-test.so", "foo");
     int a[]{1, 2, 3, 4, 5};
     double b[5];
-    executeKernel(kernel, 5, a, b);
+    kernel.execute(5, a, b);
     for(auto d : b){
       std::cout << d << std::endl;
     }
---
+++ runtime/include/spnc-runtime.h
@@ -0,0 +1,14 @@
+//
+// Created by lukas on 22.11.19.
+//
+
+#ifndef SPNC_RUNTIME_H
+#define SPNC_RUNTIME_H
+
+// Convenience header to include spnc runtime functionality with a single include with a meaningful name.
+
+#include <string>
+#include "Kernel.h"
+
+
+#endif //SPNC_RUNTIME_H
