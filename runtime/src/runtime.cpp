800292f4ca189487f17a1cf44183db9e8c9a7aeb
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2020-01-18 20:28:38 +0100

    Move Kernel class from runtime to compiler;

--- runtime/src/runtime.cpp
+++ runtime/src/runtime.cpp
@@ -4,6 +4,10 @@
 
 #include "../include/runtime.h"
 
-Kernel loadKernel(const std::string& fileName, const std::string& kernelName){
+namespace spnc {
+
+    Kernel loadKernel(const std::string& fileName, const std::string& kernelName){
+
+    }
 
 }
