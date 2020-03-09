1306d42f0e8752d87c56b752efb8cb19bcd50d73
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2020-03-09 12:21:21 +0100

    WIP: More work on the option & configuration interface;

--- compiler/include/driver/Options.h
+++ compiler/include/driver/Options.h
@@ -7,125 +7,208 @@
 #define SPNC_COMPILER_INCLUDE_DRIVER_OPTIONS_H
 
 #include <unordered_map>
+#include <iostream>
+#include <memory>
+#include <llvm/ADT/Optional.h>
+#include <algorithm>
+#include <map>
+
 namespace spnc {
 
-  class OptValue {};
+  namespace interface {
 
-  template<typename Value>
-  class OptionValue : public OptValue {
+    class OptValue {
+    public:
+      virtual ~OptValue() = default;
+    };
 
-  public:
-    explicit OptionValue(Value v) : value{v} {}
+    template<typename Value>
+    class OptionValue : public OptValue {
 
-    Value get() {
-      return value;
-    }
+    public:
+      explicit OptionValue(Value v) : value{v} {}
 
-  private:
-    Value value;
-
-  };
-
-  namespace detail {
-
-    class OptionParsers {
-
-      template<typename Value>
-      static Value parse(const std::string& value) {
-        // As a default, try to construct a the value from a string.
-        return Value{value};
-      }
-
-      template<>
-      bool parse(const std::string& value) {
-        std::string v = value;
-        std::transform(v.begin(), v.end(), v.begin(),
-                       [](unsigned char c) { return std::tolower(c); });
-        return v == "true" || v == "yes";
-      }
-
-      template<>
-      int parse(const std::string& value) {
-        return std::stoi(value);
-      }
-
-      template<>
-      double parse(const std::string& value) {
-        return std::stod(value);
-      }
-
-      template<>
-      std::string parse(const std::string& value) {
+      Value get() {
         return value;
       }
+
+    private:
+      Value value;
+
+    };
+
+    namespace detail {
+
+      class OptionParsers {
+
+      public:
+
+        template<typename Value>
+        static Value parse(const std::string& value) {
+          // As a default, try to construct a the value from a string.
+          return Value{value};
+        }
+
+        template<>
+        bool parse(const std::string& value) {
+          std::string v = value;
+          std::transform(v.begin(), v.end(), v.begin(),
+                         [](unsigned char c) { return std::tolower(c); });
+          return v == "true" || v == "yes";
+        }
+
+        template<>
+        int parse(const std::string& value) {
+          return std::stoi(value);
+        }
+
+        template<>
+        double parse(const std::string& value) {
+          return std::stod(value);
+        }
+
+        template<>
+        std::string parse(const std::string& value) {
+          return value;
+        }
+
+      };
+
+    }
+
+    class Configuration {
+
+    public:
+
+      void push_back(const std::string& key, std::unique_ptr<OptValue> value) {
+        config.emplace(key, std::move(value));
+      }
+
+      OptValue& get(const std::string& key) const {
+        return *config.at(key);
+      }
+
+      bool hasOption(const std::string& key) const {
+        return config.count(key);
+      }
+
+    private:
+
+      std::unordered_map<std::string, std::unique_ptr<OptValue>> config;
+
+    };
+
+    class Opt {
+
+    public:
+
+      explicit Opt(std::string key) : keyName{std::move(key)} {}
+
+      virtual llvm::Optional<std::unique_ptr<OptValue>> parse(const std::string& key, const std::string& value) = 0;
+
+    protected:
+
+      std::string keyName;
+
+    };
+
+    class Options {
+
+    public:
+
+      static void addOption(const std::string& key, Opt* opt) {
+        options.emplace(key, opt);
+      }
+
+      static void dump() {
+        std::cout << "Available options: " << std::endl;
+        for (auto& k : options) {
+          std::cout << k.first << std::endl;
+        }
+      }
+
+      static std::unique_ptr<Configuration> parse(const std::map<std::string, std::string>& input) {
+        auto config = std::make_unique<Configuration>();
+        for (auto& o : input) {
+          auto key = o.first;
+          auto value = o.second;
+          if (!options.count(key)) {
+            throw std::runtime_error("Unknown compile option!");
+          }
+          auto parser = options.at(key);
+          auto parseResult = parser->parse(key, value);
+          if (!parseResult) {
+            std::cout << "Could not parse option value" << value << " for " << key << std::endl;
+          }
+          config->push_back(key, std::move(parseResult.getValue()));
+        }
+        return config;
+      }
+
+      // DANGER ZONE: Correct static initialization order required.
+      static std::unordered_map<std::string, Opt*> options;
+
+    };
+
+    template<typename Value>
+    class Option : public Opt {
+
+    public:
+
+      explicit Option(std::string k) noexcept : Opt{std::move(k)} {
+        Options::addOption(keyName, this);
+      }
+
+      Option(std::string k, Value defaultVal) noexcept : Opt{std::move(k)} {
+        hasDefault = true;
+        defaultValue = defaultVal;
+        Options::addOption(keyName, this);
+      }
+
+      llvm::Optional<std::unique_ptr<OptValue>> parse(const std::string& key,
+                                                      const std::string& value) override {
+        if (key != keyName) {
+          return llvm::None;
+        }
+        std::unique_ptr<OptValue>
+            result = std::make_unique<OptionValue<Value>>(detail::OptionParsers::parse<Value>(value));
+        return result;
+      }
+
+      bool isPresent(const Configuration& config) {
+        return config.hasOption(keyName);
+      }
+
+      Value get(const Configuration& config) {
+        if (config.hasOption(keyName)) {
+          return getVal(config);
+        }
+        if (hasDefault) {
+          return defaultValue;
+        }
+        throw std::runtime_error("Trying to get value of non-present option!");
+      }
+
+      Value getOrElse(const Configuration& config, Value elseValue) {
+        if (config.hasOption(keyName)) {
+          return getVal(config);
+        }
+        return elseValue;
+      }
+
+    private:
+
+      Value getVal(const Configuration& config) {
+        return dynamic_cast<OptionValue<Value>&>(config.get(keyName)).get();
+      }
+
+      bool hasDefault = false;
+
+      Value defaultValue;
 
     };
 
   }
-
-  class Configuration {
-
-  public:
-
-    void push_back(const std::string& key, std::unique_ptr<OptValue> value) {
-      config.emplace(key, std::move(value));
-    }
-
-    OptValue& get(const std::string& key) {
-      return *config[key];
-    }
-
-  private:
-
-    std::unordered_map<std::string, std::unique_ptr<OptValue>> config;
-
-  };
-
-  class Opt {
-
-  public:
-
-    explicit Opt(std::string key) : keyName{std::move(key)} {}
-
-    virtual llvm::Optional<OptValue> parse(const std::string& key, const std::string& value) = 0;
-
-  protected:
-
-    std::string keyName;
-
-  };
-
-  class Options {
-
-  public:
-
-    static void addOption(const std::string& key, Opt* opt) {
-      options.emplace(key, opt);
-    }
-
-  private:
-
-    static std::unordered_map<std::string, Opt*> options;
-
-  };
-
-  template<typename Value>
-  class Option : public Opt {
-
-  public:
-    explicit Option(std::string k) : Opt{std::move(k)} {
-      Options::addOption(keyName, this);
-    }
-
-    llvm::Optional<std::unique_ptr<OptionValue<Value>>> parse(const std::string& key,
-                                                              const std::string& value) override {
-      if (key != keyName) {
-        return llvm::None;
-      }
-      return std::make_unique<OptionValue<Value>>(detail::OptionParsers::parse<Value>(value));
-    }
-
-  };
 
 }
 
---
+++ compiler/src/driver/option/Options.cpp
@@ -0,0 +1,10 @@
+//
+// This file is part of the SPNC project.
+// Copyright (c) 2020 Embedded Systems and Applications Group, TU Darmstadt. All rights reserved.
+//
+
+#include <driver/Options.h>
+
+using namespace spnc::interface;
+
+std::unordered_map<std::string, Opt*> Options::options;
