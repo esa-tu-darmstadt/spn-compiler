4cadca8db4bfc0655868fe208e51f3c5e3b8fb84
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2020-10-30 08:42:38 +0100

    Make lowering of Histograms to LLVM dialect type-agnostic;

--- mlir-dialects/lib/Conversion/SPNtoLLVM/SPNtoLLVMPatterns.cpp
+++ mlir-dialects/lib/Conversion/SPNtoLLVM/SPNtoLLVMPatterns.cpp
@@ -11,11 +11,14 @@
 mlir::LogicalResult mlir::spn::HistogramOpLowering::matchAndRewrite(mlir::spn::HistogramOp op,
                                                                     llvm::ArrayRef<mlir::Value> operands,
                                                                     mlir::ConversionPatternRewriter& rewriter) const {
+  // Simple count for unique naming of global arrays.
   static int histCount = 0;
 
+  // Check for single operand, i.e. the index value.
   assert(operands.size() == 1);
 
-  // Collect all mappings from input var value to probability value in a map.
+  // Collect all mappings from input var value to probability value in a map
+  // and compute the minimum lower bound & maximum upper bound.
   llvm::DenseMap<int, double> values;
   int minLB = std::numeric_limits<int>::max();
   int maxUB = std::numeric_limits<int>::min();
@@ -36,20 +39,35 @@ mlir::LogicalResult mlir::spn::HistogramOpLowering::matchAndRewrite(mlir::spn::H
     return failure();
   }
 
+  auto resultType = op.getResult().getType();
+  if (!resultType.isIntOrFloat()) {
+    // Currently only handling Int and Float result types.
+    return failure();
+  }
+
   // Flatten the map into an array by filling up empty indices with 0 values.
   SmallVector<Attribute, 256> valArray;
   for (int i = 0; i < maxUB; ++i) {
+    double indexVal;
     if (values.count(i)) {
-      // TODO Make type flexible based on analysis
-      valArray.push_back(rewriter.getF64FloatAttr(values[i]));
+      indexVal = values[i];
     } else {
-      valArray.push_back(rewriter.getF64FloatAttr(0));
+      // Fill up with 0 if no value was defined by the histogram.
+      indexVal = 0;
+    }
+    // Construct attribute with constant value. Need to distinguish cases here due to different builder methods.
+    if (resultType.isIntOrIndex()) {
+      valArray.push_back(rewriter.getIntegerAttr(resultType, (int) indexVal));
+    } else {
+      valArray.push_back(rewriter.getFloatAttr(resultType, indexVal));
     }
   }
   auto valArrayAttr = rewriter.getArrayAttr(valArray);
 
-  auto doubleType = LLVM::LLVMType::getDoubleTy(op.getContext());
-  auto arrType = LLVM::LLVMType::getArrayTy(doubleType, maxUB);
+  // Create & insert a constant global array with the values from the histogram.
+  auto elementType = typeConverter->convertType(resultType).dyn_cast<mlir::LLVM::LLVMType>();
+  assert(elementType);
+  auto arrType = LLVM::LLVMType::getArrayTy(elementType, maxUB);
   auto module = op.getParentOfType<ModuleOp>();
   auto restore = rewriter.saveInsertionPoint();
   rewriter.setInsertionPointToStart(module.getBody());
@@ -61,10 +79,11 @@ mlir::LogicalResult mlir::spn::HistogramOpLowering::matchAndRewrite(mlir::spn::H
                                                      valArrayAttr);
   rewriter.restoreInsertionPoint(restore);
 
+  // Load a value from the histogram using the index value to index into the histogram.
   auto addressOf = rewriter.create<LLVM::AddressOfOp>(op.getLoc(), globalConst);
   auto indexType = LLVM::LLVMType::getInt64Ty(rewriter.getContext());
   auto constZeroIndex = rewriter.create<LLVM::ConstantOp>(op.getLoc(), indexType, rewriter.getI64IntegerAttr(0));
-  auto ptrType = doubleType.getPointerTo();
+  auto ptrType = elementType.getPointerTo();
   auto gep = rewriter.create<LLVM::GEPOp>(op.getLoc(), ptrType, addressOf, ValueRange{constZeroIndex, operands[0]});
   rewriter.replaceOpWithNewOp<LLVM::LoadOp>(op, gep);
   return success();
