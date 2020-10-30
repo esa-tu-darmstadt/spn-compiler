26e7b74ef9dd18eed7ca35906b7c332a5b544178
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2020-10-30 09:21:53 +0100

    Common base for MLIR pass pipelines using CRTP idiom;

--- compiler/src/codegen/mlir/pipeline/SPNDialectPipeline.cpp
+++ compiler/src/codegen/mlir/pipeline/SPNDialectPipeline.cpp
@@ -13,29 +13,10 @@
 using namespace mlir;
 using namespace spnc;
 
-SPNDialectPipeline::SPNDialectPipeline(ActionWithOutput<mlir::ModuleOp>& _input,
-                                       std::shared_ptr<mlir::MLIRContext> _mlirContext)
-    : ActionSingleInput<ModuleOp, ModuleOp>{_input},
-      mlirContext{std::move(_mlirContext)}, pm{mlirContext.get()} {
-  pm.addPass(mlir::spn::createSPNOpSimplifierPass());
-  pm.addPass(mlir::createCanonicalizerPass());
-  pm.addPass(mlir::spn::createSPNTypePinningPass());
-  pm.addPass(mlir::spn::createSPNtoStandardConversionPass());
-  pm.addPass(mlir::spn::createSPNtoLLVMConversionPass());
-}
-
-ModuleOp& SPNDialectPipeline::execute() {
-  if (!cached) {
-    auto inputModule = input.execute();
-    // Clone the module to keep the original module available
-    // for actions using the same input module.
-    module = std::make_unique<ModuleOp>(inputModule.clone());
-    auto result = pm.run(*module);
-    if (failed(result)) {
-      SPNC_FATAL_ERROR("Running the MLIR pass pipeline failed!");
-    }
-    cached = true;
-  }
-  module->dump();
-  return *module;
+void SPNDialectPipeline::initializePassPipeline(mlir::PassManager* pm, mlir::MLIRContext* ctx) {
+  pm->addPass(mlir::spn::createSPNOpSimplifierPass());
+  pm->addPass(mlir::createCanonicalizerPass());
+  pm->addPass(mlir::spn::createSPNTypePinningPass());
+  pm->addPass(mlir::spn::createSPNtoStandardConversionPass());
+  pm->addPass(mlir::spn::createSPNtoLLVMConversionPass());
 }
--- compiler/src/codegen/mlir/pipeline/SPNDialectPipeline.h
+++ compiler/src/codegen/mlir/pipeline/SPNDialectPipeline.h
@@ -6,35 +6,19 @@
 #ifndef SPNC_COMPILER_SRC_CODEGEN_MLIR_PIPELINE_SPNDIALECTPIPELINE_H
 #define SPNC_COMPILER_SRC_CODEGEN_MLIR_PIPELINE_SPNDIALECTPIPELINE_H
 
-#include <driver/Actions.h>
-#include <mlir/IR/Module.h>
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
+#include "../MLIRPassPipeline.h"
 
 namespace spnc {
 
   ///
   /// Action running a series of MLIR passes on the SPN dialect.
-  class SPNDialectPipeline : public ActionSingleInput<mlir::ModuleOp, mlir::ModuleOp> {
+  class SPNDialectPipeline : public MLIRPipelineBase<SPNDialectPipeline> {
 
   public:
 
-    /// Constructor.
-    /// \param _input Action providing the input MLIR module.
-    /// \param _mlirContext Surrounding MLIR context.
-    SPNDialectPipeline(ActionWithOutput<mlir::ModuleOp>& _input, std::shared_ptr<mlir::MLIRContext> _mlirContext);
+    using MLIRPipelineBase<SPNDialectPipeline>::MLIRPipelineBase;
 
-    mlir::ModuleOp& execute() override;
-
-  private:
-
-    std::shared_ptr<mlir::MLIRContext> mlirContext;
-
-    bool cached = false;
-
-    mlir::PassManager pm;
-
-    std::unique_ptr<mlir::ModuleOp> module;
+    void initializePassPipeline(mlir::PassManager* pm, mlir::MLIRContext* ctx);
 
   };
 
