a600daef517490ca2e2c201c70f730d134fdfed7
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2021-03-11 08:58:49 +0100

    Add passes to complete bufferization to LoSPNtoGPUConversion;

--- compiler/src/codegen/mlir/conversion/LoSPNtoGPUConversion.cpp
+++ compiler/src/codegen/mlir/conversion/LoSPNtoGPUConversion.cpp
@@ -11,4 +11,14 @@ void spnc::LoSPNtoGPUConversion::initializePassPipeline(mlir::PassManager* pm, m
   pm->addPass(mlir::spn::createLoSPNtoGPUStructureConversionPass());
   pm->addPass(mlir::createGpuKernelOutliningPass());
   pm->addPass(mlir::spn::createLoSPNtoGPUNodeConversionPass());
+  // The remaining bufferization, buffer deallocation and copy removal passes
+  // currently need to be placed at this point in the pipeline, as they operate
+  // on FuncOp (not SPNKernel/SPNTask) and can therefore only run after the
+  // conversion to FuncOp. This could be avoided at least for Kernels by
+  // converting them to FuncOp earlier in the pipeline, e.g., during
+  // bufferization of Kernels.
+  pm->nest<mlir::FuncOp>().addPass(mlir::createTensorBufferizePass());
+  pm->nest<mlir::FuncOp>().addPass(mlir::createFinalizingBufferizePass());
+  pm->nest<mlir::FuncOp>().addPass(mlir::createBufferDeallocationPass());
+  pm->addPass(mlir::createCopyRemovalPass());
 }
