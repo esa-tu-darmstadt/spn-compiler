b0eba0fd34f4af658f500b18e3270b28aa3f56eb
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2021-03-12 13:55:57 +0100

    Make LoSPN to GPU conversion depend on CUDA; Make device information accessible;

---
+++ CudaToolkit.cmake
@@ -0,0 +1,20 @@
+macro(cuda_setup)
+    # TODO: The old FindCUDA is deprecated, the new FindCUDAToolkit is only available in version > 3.17.
+    # Use this feature here after upgrading the required CMake version.
+    find_package(CUDA)
+    if (NOT CUDA_FOUND)
+        message(FATAL_ERROR "Targeting CUDA GPUs requires a working CUDA install, try setting CUDA_TOOLKIT_ROOT_DIR")
+    else ()
+        message(STATUS "Using CUDA ${CUDA_VERSION_MAJOR}.${CUDA_VERSION_MINOR} from ${CUDA_TOOLKIT_ROOT_DIR}")
+        message(STATUS "Using CUDA runtime headers: " ${CUDA_INCLUDE_DIRS})
+    endif ()
+
+    # When compiling for CUDA GPUs, the compiler will invoke the PTX compiler and linker
+    # through the CUDA runtime library API.
+    find_library(CUDA_RUNTIME_LIBRARY cuda)
+    if (NOT CUDA_RUNTIME_LIBRARY)
+        message(FATAL_ERROR "CUDA runtime library not found. Set location manually through CUDA_RUNTIME_LIBRARY")
+    else ()
+        message(STATUS "Using CUDA runtime library: " ${CUDA_RUNTIME_LIBRARY})
+    endif ()
+endmacro(cuda_setup)
--- compiler/DetectCUDALibs.cmake
+++ compiler/DetectCUDALibs.cmake
@@ -1,23 +1,4 @@
-macro(cuda_setup)
-    # TODO: The old FindCUDA is deprecated, the new FindCUDAToolkit is only available in version > 3.17.
-    # Use this feature here after upgrading the required CMake version.
-    find_package(CUDA)
-    if (NOT CUDA_FOUND)
-        message(FATAL_ERROR "Targeting CUDA GPUs requires a working CUDA install, try setting CUDA_TOOLKIT_ROOT_DIR")
-    else ()
-        message(STATUS "Using CUDA ${CUDA_VERSION_MAJOR}.${CUDA_VERSION_MINOR} from ${CUDA_TOOLKIT_ROOT_DIR}")
-        message(STATUS "Using CUDA runtime headers: " ${CUDA_INCLUDE_DIRS})
-    endif ()
-
-    # When compiling for CUDA GPUs, the compiler will invoke the PTX compiler and linker
-    # through the CUDA runtime library API.
-    find_library(CUDA_RUNTIME_LIBRARY cuda)
-    if (NOT CUDA_RUNTIME_LIBRARY)
-        message(FATAL_ERROR "CUDA runtime library not found. Set location manually through CUDA_RUNTIME_LIBRARY")
-    else ()
-        message(STATUS "Using CUDA runtime library: " ${CUDA_RUNTIME_LIBRARY})
-    endif ()
-
+macro(detect_cuda_libs)
     # When compiling for CUDA GPUs, the LLVM IR generated for GPU kernels must be linked
     # with libdevice, a Nvidia-provided bitcode library with optimized device functions (e.g. log, exp).
     if (NOT SPNC_LIBDEVICE)
@@ -37,7 +18,6 @@ macro(cuda_setup)
     endif ()
     message(STATUS "Using CUDA libdevice from: " ${SPNC_LIBDEVICE})
 
-
     # When compiling for CUDA GPUs, the generated Kernel must be linked with the
     # MLIR CUDA runtime wrappers for data-transfer, kernel-launch etc.
     find_library(MLIR_CUDA_RUNTIME_WRAPPERS cuda-runtime-wrappers
@@ -47,4 +27,4 @@ macro(cuda_setup)
     else ()
         message(STATUS "Using MLIR CUDA wrappers from:" ${MLIR_CUDA_RUNTIME_WRAPPERS})
     endif ()
-endmacro(cuda_setup)
+endmacro(detect_cuda_libs)
