fb1fea430bcb945bb918632ffb7f76b5a0d887da
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2021-03-18 13:29:26 +0100

    Capture native execution time in runtime;

--- runtime/src/Executable.cpp
+++ runtime/src/Executable.cpp
@@ -8,6 +8,7 @@
 #include <util/Logging.h>
 #include "Executable.h"
 #include <omp.h>
+#include <chrono>
 
 using namespace spnc_rt;
 
@@ -65,12 +66,13 @@ void Executable::executeBatch(size_t num_samples, void* inputs, void* outputs) {
   char* input_ptr = reinterpret_cast<char*>(inputs);
   char* output_ptr = reinterpret_cast<char*>(outputs);
   size_t batchSize = kernel->batchSize();
-#pragma omp parallel for firstprivate(input_ptr, output_ptr, batchSize, num_samples)
+  auto nativeStart = std::chrono::high_resolution_clock::now();
+//#pragma omp parallel for firstprivate(input_ptr, output_ptr, batchSize, num_samples)
   for (size_t i = 0; i < num_samples; i += batchSize) {
     // Calculate the number of remaining samples, can be < batchSize for the last batch.
     size_t remainingSamples = num_samples - i;
     size_t samples = std::min(batchSize, remainingSamples);
-    std::cout << "Remaining samples: " << remainingSamples << " now computing: " << samples << std::endl;
+    //std::cout << "Remaining samples: " << remainingSamples << " now computing: " << samples << std::endl;
     // Calculate pointer to first input of this batch, using information about the
     // number of features and number of bytes used to encode the feature.
     char* input_offset = &(input_ptr[i * kernel->numFeatures() * kernel->bytesPerFeature()]);
@@ -80,6 +82,9 @@ void Executable::executeBatch(size_t num_samples, void* inputs, void* outputs) {
     kernel_func(input_offset, input_offset, 0, samples, 1, kernel->numFeatures(), 1,
                 output_offset, output_offset, 0, samples, 1);
   }
+  auto nativeEnd = std::chrono::high_resolution_clock::now();
+  auto nativeTime = std::chrono::duration_cast<std::chrono::nanoseconds>(nativeEnd - nativeStart);
+  std::cout << "SPEAKER_IDENT: NATIVE EXECUTION TIME: " << nativeTime.count() << " ns" << std::endl;
 }
 
 void Executable::executeGPU(size_t num_samples, void* inputs, void* outputs) {
