478cdf0bc0cc03a6ed60e30fe199fd6c4ef497a4
CSV <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2021-04-05 20:33:51 +0200

    Fix: flawed LoadOp detection.

--- mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/SLPMode.cpp
+++ mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/SLPMode.cpp
@@ -4,6 +4,7 @@
 //
 
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "LoSPN/LoSPNOps.h"
 #include "LoSPNtoCPU/Vectorization/SLP/SLPMode.h"
 
 using namespace mlir;
@@ -12,7 +13,7 @@ using namespace mlir::spn::low::slp;
 Mode mlir::spn::low::slp::modeFromOperation(const Operation* operation) {
   if (dyn_cast<ConstantOp>(operation)) {
     return CONST;
-  } else if (dyn_cast<LoadOp>(operation)) {
+  } else if (dyn_cast<mlir::spn::low::SPNBatchRead>(operation)) {
     return LOAD;
   }
   return OPCODE;
