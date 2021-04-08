4a01056c30c94e63da8a9a8911eb4e0de1ea8112
Lukas Sommer <sommer@esa.tu-darmstadt.de>
2021-04-08 15:10:19 +0200

    Add Python package documentation and metadata;

--- xspn/setup.py
+++ xspn/setup.py
@@ -1,14 +1,28 @@
 import setuptools
+import os
 
 with open("requirements.txt", "r") as rf:
     requirements = rf.readlines()
 
+readme = os.path.join(os.path.realpath(os.path.dirname(__file__)), "..", "README.md")
+with open(readme, "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="xspn",
-    version="0.0.1",
+    version="0.1",
     author="Embedded Systems and Applications Group, TU Darmstadt",
     author_email="sommer@esa.tu-darmstadt.de",
     description="XSPN: Library bridging between SPFlow and the SPN compiler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/esa-tu-darmstadt/spn-compiler",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ],
     packages=setuptools.find_packages(include=["xspn", "xspn.*"]),
     package_data={"xspn.serialization.binary" : ["capnproto/*.capnp"]},
     install_requires=requirements,
