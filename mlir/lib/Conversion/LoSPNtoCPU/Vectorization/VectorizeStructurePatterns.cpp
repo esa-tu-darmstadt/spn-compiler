399200f130025abe1ca8b4d4160b63977ea89ad0
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2021-05-12 21:07:24 +0200

    Add timing and debug info.

--- mlir/lib/Conversion/LoSPNtoCPU/Vectorization/VectorizeStructurePatterns.cpp
+++ mlir/lib/Conversion/LoSPNtoCPU/Vectorization/VectorizeStructurePatterns.cpp
@@ -124,7 +124,7 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
   }
   // Inline the content of the task into the function.
   rewriter.mergeBlocks(&task.body().front(), taskBlock, blockReplacementArgs);
-
+  llvm::DebugFlag = true;
   // Apply SLP vectorization.
   //task->getParentOfType<ModuleOp>()->dump();
   task->emitRemark() << "Beginning SLP vectorization";
@@ -166,13 +166,22 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
   auto fivePercent = std::ceil(static_cast<double>(numVectors) * 0.05);
   unsigned fivePercentCounter = 0;
 
+  auto start = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> timeSpentMatching{};
+  std::chrono::duration<double> timeSpentCreatingLeaves{};
+  std::chrono::duration<double> timeSpentExtractingOperands{};
+  std::chrono::duration<double> timeSpentExtractingEscaping{};
+
   // Traverse the SLP graph and apply the vectorization patterns.
   auto const& order = conversionManager.conversionOrder();
   for (size_t n = 0; n < order.size(); ++n) {
     vector = order[n];
     //dumpSLPNodeVector(*vector);
     auto* vectorOp = vector->begin()->getDefiningOp();
+    auto matchingStart = std::chrono::high_resolution_clock::now();
     if (vector->containsBlockArgs() || failed(applicator.matchAndRewrite(vectorOp, rewriter))) {
+      timeSpentMatching +=  std::chrono::high_resolution_clock::now() - matchingStart;
+      auto operandExtracting = std::chrono::high_resolution_clock::now();
       auto const& vectorType = VectorType::get(static_cast<unsigned>(vector->numLanes()), computationType);
       conversionManager.setInsertionPointFor(vector);
       // Create extractions from vectorized operands if present.
@@ -192,6 +201,8 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
           break;
         }
       }
+      timeSpentExtractingOperands += std::chrono::high_resolution_clock::now() - operandExtracting;
+      auto leafCreating = std::chrono::high_resolution_clock::now();
       if (vector->splattable()) {
         auto const& element = vector->getElement(0);
         auto vectorizedOp = rewriter.create<vector::BroadcastOp>(element.getLoc(), vectorType, element);
@@ -200,8 +211,12 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
         auto vectorizedOp = broadcastFirstInsertRest(vector, vectorType, rewriter, conversionManager);
         conversionManager.update(vector, vectorizedOp, ElementFlag::KeepAll);
       }
+      timeSpentCreatingLeaves += std::chrono::high_resolution_clock::now() - leafCreating;
+    } else {
+      timeSpentMatching += std::chrono::high_resolution_clock::now() - matchingStart;
     }
     // Create vector extractions for escaping uses & erase superfluous operations.
+    auto extractionEscaping = std::chrono::high_resolution_clock::now();
     auto const& creationMode = conversionManager.getElementFlag(vector);
     for (size_t lane = 0; lane < vector->numLanes(); ++lane) {
       auto const& element = vector->getElement(lane);
@@ -227,12 +242,20 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
         element.replaceAllUsesWith(extractOp.result());
       }
       rewriter.eraseOp(element.getDefiningOp());
+
     }
+    timeSpentExtractingEscaping += std::chrono::high_resolution_clock::now() - extractionEscaping;
     if (static_cast<double>(n) >= (fivePercentCounter * fivePercent)) {
       task->emitRemark("Conversion progress: " + std::to_string(5 * fivePercentCounter++) + '%');
     }
   }
+  std::chrono::duration<double> timeSpent = std::chrono::high_resolution_clock::now() - start;
   task->emitRemark("Graph conversion complete.");
+  task->emitRemark("Overall time spent (s): " + std::to_string(timeSpent.count()));
+  task->emitRemark("\tTime spent matching & applying patterns (s): " + std::to_string(timeSpentMatching.count()));
+  task->emitRemark("\tTime spent creating leaves (s): " + std::to_string(timeSpentCreatingLeaves.count()));
+  task->emitRemark("\tTime spent creating operand extractions (s): " + std::to_string(timeSpentExtractingOperands.count()));
+  task->emitRemark("\tTime spent creating escaping use extractions (s): " + std::to_string(timeSpentExtractingEscaping.count()));
   rewriter.restoreInsertionPoint(callPoint);
   rewriter.replaceOpWithNewOp<CallOp>(task, taskFunc, operands);
   taskFunc.body().walk([&](Operation* op) {
@@ -245,6 +268,7 @@ LogicalResult VectorizeSingleTask::matchAndRewrite(SPNTask task,
     }
     return WalkResult::advance();
   });
+  llvm::DebugFlag = false;
   return success();
 }
 
