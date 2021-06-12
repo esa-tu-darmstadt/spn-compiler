6d776ad98fd7bcfdcc36c361346e58f8636b5a4c
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2021-06-12 16:24:07 +0200

    Improve run_all script.

--- run_all.py
+++ run_all.py
@@ -15,13 +15,22 @@ from spnc.cpu import CPUCompiler
 from xspn.serialization.binary.BinarySerialization import BinaryDeserializer
 
 
-def test_spn(spn, inputs, reference, spn_name, log_file):
+def read_csv(csv_file, delimiter=",", dtype="float64", max_rows=None):
+    if not os.path.isfile(csv_file):
+        raise Exception("CSV file does not exist")
+    data = np.genfromtxt(fname=csv_file, delimiter=delimiter, dtype=dtype, max_rows=max_rows)
+    if data.ndim < 2:
+        data = np.atleast_2d(data)
+    return data
+
+
+def test_spn(spn, inputs, references, spn_name, log_file):
     # Things to log.
     log_dict = {
         "name": spn_name,
         "execution time (s)": 0,
         "compile time (s)": 0,
-        "num_evaluations": reference.size
+        "num_evaluations": references.size
     }
     # Compile the kernel.
     compiler = CPUCompiler(vectorize=False, computeInLogSpace=True)
@@ -29,13 +38,13 @@ def test_spn(spn, inputs, reference, spn_name, log_file):
     kernel = compiler.compile_ll(spn=spn, batchSize=1, supportMarginal=False)
     log_dict["compile time (s)"] = time.time() - start
     # Execute the compiled kernel.
-    for i in range(reference.size):
+    for i in range(references.size):
         # Check the computation results against the reference
+        reference = references[i]
         start = time.time()
-        result = compiler.execute(kernel, inputs=np.array([inputs[i]]))
+        result = compiler.execute(kernel, inputs=np.atleast_2d(inputs[i]))
         log_dict["execution time (s)"] = log_dict["execution time (s)"] + time.time() - start
-        assert np.isclose(result,
-                          reference[i]), f"evaluation #{i}: result: {result[0]:16.9f}, reference: {reference[i]:16.8f}"
+        assert np.isclose(result, reference), f"evaluation #{i}: result: {result:16.9f}, reference: {reference:16.8f}"
     file_exists = os.path.isfile(log_file)
     with open(log_file, 'a') as csvfile:
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=log_dict.keys())
@@ -44,15 +53,15 @@ def test_spn(spn, inputs, reference, spn_name, log_file):
         writer.writerow(log_dict)
 
 
-def test_all_speakers(models_path, io_path, log_file, max_rows=None):
+def test_speakers(models_path, io_path, log_file, max_rows=None):
     if not os.path.isdir(models_path):
         raise Exception("Must provide valid path to models directory")
     if not os.path.isdir(io_path):
         raise Exception("Must provide valid path to IO files directory")
-    input_csv = os.path.join(io_path, "input.csv")
-    if not os.path.isfile(input_csv):
-        raise Exception("No input.csv file in IO files directory")
-    inputs = np.genfromtxt(input_csv, delimiter=",", dtype="float64", max_rows=max_rows)
+    inputs = read_csv(os.path.join(io_path, "input.csv"),
+                      delimiter=",",
+                      dtype="float64",
+                      max_rows=max_rows)
     for path, subdirectories, files in os.walk(models_path):
         print("Found", len(files), "models in folder", path)
         for filename in files:
@@ -60,17 +69,34 @@ def test_all_speakers(models_path, io_path, log_file, max_rows=None):
             print("\t> SPN:", spn_name)
             query = BinaryDeserializer(os.path.join(path, filename)).deserialize_from_file()
             spn = query.root
-            reference = np.genfromtxt(fname=os.path.join(io_path, spn_name + ".csv"),
-                                      delimiter=",",
-                                      dtype="float64",
-                                      max_rows=max_rows)
-            test_spn(spn=spn, inputs=inputs, reference=reference, log_file=log_file, spn_name=spn_name)
+            references = read_csv(os.path.join(io_path, spn_name + ".csv"),
+                                  delimiter=",",
+                                  dtype="float64",
+                                  max_rows=max_rows)
+            references = references.reshape(references.size)
+            test_spn(spn=spn, inputs=inputs, references=references, log_file=log_file, spn_name=spn_name)
+
+
+def test_all_speakers(log_prefix, max_rows=None):
+    speaker_models = "/net/celebdil/spn-benchmarks/speaker-identification/models/"
+    test_speakers(models_path=speaker_models,
+                  io_path="/net/celebdil/spn-benchmarks/speaker-identification/io_clean/",
+                  max_rows=max_rows,
+                  log_file=log_prefix + "_clean.log")
+    test_speakers(models_path=speaker_models,
+                  io_path="/net/celebdil/spn-benchmarks/speaker-identification/io_noisy_marg_0_bounds_0/",
+                  max_rows=max_rows,
+                  log_file=log_prefix + "_io_noisy_marg_0_bounds_0.log")
+    test_speakers(models_path=speaker_models,
+                  io_path="/net/celebdil/spn-benchmarks/speaker-identification/io_noisy_marg_1_bounds_0/",
+                  max_rows=max_rows,
+                  log_file=log_prefix + "_io_noisy_marg_1_bounds_0.log")
+    test_speakers(models_path=speaker_models,
+                  io_path="/net/celebdil/spn-benchmarks/speaker-identification/io_noisy_marg_1_bounds_1/",
+                  max_rows=max_rows,
+                  log_file=log_prefix + "_io_noisy_marg_1_bounds_1.log")
 
 
 if __name__ == "__main__":
-    speaker_models = "/net/celebdil/spn-benchmarks/speaker-identification/models/"
-    test_all_speakers(models_path=speaker_models,
-                      io_path="/net/celebdil/spn-benchmarks/speaker-identification/io_clean/",
-                      max_rows=1000,
-                      log_file="normal.log")
+    test_all_speakers(log_prefix="speakers", max_rows=10)
     print("DONE")
