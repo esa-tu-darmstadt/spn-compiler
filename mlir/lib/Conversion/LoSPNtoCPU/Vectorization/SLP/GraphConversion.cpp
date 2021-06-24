eb96d69f4409bfdf12474d3a90923c0062f537cc
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2021-06-25 00:06:53 +0200

    Fix: only attach log when needed.

--- mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/GraphConversion.cpp
+++ mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/GraphConversion.cpp
@@ -348,12 +348,12 @@ void ConversionManager::update(Superword* superword, Value operation, SLPVectori
     if (hasEscapingUsers(element)) {
       Value extractOp = getOrExtractValue(element);
       if (extractOp != element) {
-        if (extractOp.getType().isa<LogType>()) {
-          extractOp = rewriter.create<SPNAttachLog>(element.getLoc(), extractOp, extractOp.getType());
-        }
         for (auto* escapingUser : escapingUsers.lookup(element)) {
           for (size_t i = 0; i < escapingUser->getNumOperands(); ++i) {
             if (escapingUser->getOperand(i) == element) {
+              if (escapingUser->getOperand(i).getType().isa<LogType>()) {
+                extractOp = rewriter.create<SPNAttachLog>(element.getLoc(), extractOp, extractOp.getType());
+              }
               escapingUser->setOperand(i, extractOp);
             }
           }
