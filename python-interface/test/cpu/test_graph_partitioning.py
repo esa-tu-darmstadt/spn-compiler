47beb840d65f2d09009116c1aae16ab858d22c8d
Michael Halkenhäuser <michaelhalk@web.de>
2021-08-05 13:50:51 +0200

    [FIX] Adapted Python tests.  - Now, they will not(!) be eligible for "select-transformation".   (New tests will be added in the next commit.)

--- python-interface/test/cpu/test_graph_partitioning.py
+++ python-interface/test/cpu/test_graph_partitioning.py
@@ -17,9 +17,9 @@ from spnc.cpu import CPUCompiler
 
 def test_cpu_histogram():
     # Construct a minimal SPN.
-    h1 = Histogram([0., 1., 2.], [0.25, 0.75], [1, 1], scope=0)
+    h1 = Histogram([0., 1., 3.], [0.25, 0.75], [1, 1], scope=0)
     h2 = Histogram([0., 3., 6., 8.], [0.45, 0.1, 0.55], [1, 1], scope=1)
-    h3 = Histogram([0., 1., 2.], [0.33, 0.67], [1, 1], scope=0)
+    h3 = Histogram([0., 1., 4.], [0.33, 0.67], [1, 1], scope=0)
     h4 = Histogram([0., 5., 8.], [0.875, 0.125], [1, 1], scope=1)
 
     p0 = Product(children=[h1, h2])
--- python-interface/test/cpu/test_marginal_cpu_histogram.py
+++ python-interface/test/cpu/test_marginal_cpu_histogram.py
@@ -17,10 +17,10 @@ from spnc.cpu import CPUCompiler
 
 def test_cpu_histogram():
     # Construct a minimal SPN.
-    h1 = Histogram([0., 1., 2.], [0.25, 0.75], [1, 1], scope=0)
-    h2 = Histogram([0., 1., 2.], [0.45, 0.55], [1, 1], scope=1)
-    h3 = Histogram([0., 1., 2.], [0.33, 0.67], [1, 1], scope=0)
-    h4 = Histogram([0., 1., 2.], [0.875, 0.125], [1, 1], scope=1)
+    h1 = Histogram([0., 1., 3.], [0.25, 0.75], [1, 1], scope=0)
+    h2 = Histogram([0., 1., 3.], [0.45, 0.55], [1, 1], scope=1)
+    h3 = Histogram([0., 1., 3.], [0.33, 0.67], [1, 1], scope=0)
+    h4 = Histogram([0., 1., 3.], [0.875, 0.125], [1, 1], scope=1)
 
     p0 = Product(children=[h1, h2])
     p1 = Product(children=[h3, h4])
