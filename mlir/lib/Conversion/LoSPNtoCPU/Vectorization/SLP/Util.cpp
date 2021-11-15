3f65e782bd518e9678d67bde32f8b80b819a952f
Sebastian Vollbrecht <christoph_sebastian.vollbrecht@stud.tu-darmstadt.de>
2021-11-15 15:30:54 +0100

    Fix: change total number of vectorization attempts to 1.

--- mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/Util.cpp
+++ mlir/lib/Conversion/LoSPNtoCPU/Vectorization/SLP/Util.cpp
@@ -17,7 +17,7 @@ using namespace mlir::spn::low::slp;
 // TODO: I don't like this redefinition of default options. Maybe find a way to include GlobalOptions.h?
 unsigned option::maxNodeSize = 10;
 unsigned option::maxLookAhead = 3;
-unsigned option::maxAttempts = 5;
+unsigned option::maxAttempts = 1;
 unsigned option::maxSuccessfulIterations = 1;
 bool option::reorderInstructionsDFS = true;
 bool option::allowDuplicateElements = false;
