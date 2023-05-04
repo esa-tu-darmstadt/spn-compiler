f58962b0d8d9b5a1f9144f3f87ef848b97085775
jschj <joe.sch@protonmail.com>
2023-05-04 12:29:16 +0200

    script

--- scripts/run-cosim.sh
+++ scripts/run-cosim.sh
@@ -15,8 +15,8 @@ cd tmp_cosim
 
 $BASE_DIR/spn-compiler/build/execute/driver $BASE_DIR/spn-compiler/nips5.bin --target FPGA --controller-generator-path "" || true
 
-circt-opt output.mlir --esi-connect-services --esi-emit-collateral=schema-file=schema.capnp > x.mlir
-circt-opt x.mlir --lower-esi-to-physical --lower-esi-ports --lower-esi-to-hw --export-split-verilog -o y.mlir
-circt-translate x.mlir -export-esi-capnp -verify-diagnostics > schema.capnp
+circt-opt output.mlir --mlir-disable-threading --esi-connect-services --esi-emit-collateral=schema-file=schema.capnp > x.mlir
+#circt-opt x.mlir --lower-esi-to-physical --lower-esi-ports --lower-esi-to-hw --export-split-verilog -o y.mlir
+#circt-translate x.mlir -export-esi-capnp -verify-diagnostics > schema.capnp
 # RUN: cd ..
 # RUN: esi-cosim-runner.py --schema %t2.capnp --exec %S/loopback.py %t6/*.sv
